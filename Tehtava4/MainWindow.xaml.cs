0eb0e021f14c173abeb55c2e0f56a48db9478995
mikkoleppanen <mikko.t.leppanen@hotmail.com>
2015-09-23 23:59:11 +0300

    Tehtävä 4:sta tehty mutta filtteröinti ei toimi

--- Tehtava4/MainWindow.xaml.cs
+++ Tehtava4/MainWindow.xaml.cs
@@ -18,6 +18,7 @@ namespace Tehtava4
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    /// 
     public partial class MainWindow : Window
     {
         public MainWindow()
--- Tehtava4/Winecellar1.xaml.cs
+++ Tehtava4/Winecellar1.xaml.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,6 +13,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using System.Xml.Linq;
 
 namespace Tehtava4
 {
@@ -20,9 +23,40 @@ namespace Tehtava4
     public partial class Winecellar1 : Window
     {
         public String User = "";
+        public ObservableCollection<string> list = new ObservableCollection<string>();
         public Winecellar1()
         {
             InitializeComponent();
+
+            var xmlDoc = XDocument.Load("E:/Github/IIO13200_NET_15S/Tehtava4/Viinit1.xml").Root;
+            dgWines.DataContext = xmlDoc;
+
+            var countries = from s in xmlDoc.Descendants("wine") select s.Element("maa").Value;
+
+            foreach (String country in countries)
+            {
+                if(!list.Contains(country))
+                {
+                    list.Add(country);
+                }
+            }
+
+            this.cmbCountry.ItemsSource = list;
+        }
+
+        private void btnSort_Click(object sender, RoutedEventArgs e)
+        {
+            var xmlDoc = XDocument.Load("E:/Github/IIO13200_NET_15S/Tehtava4/Viinit1.xml").Root;
+           /* var query = from p in xmlDoc.Descendants("wine")
+                        where p.Element("maa").Value == this.cmbCountry.Text
+                        group p by p.Element("maa");*/
+
+            var query = from p in xmlDoc.Descendants("wine")
+                          where p.Element("maa").Value == this.cmbCountry.Text
+                          select p;
+
+            this.dgWines.DataContext = query;
+            this.dgWines.Items.Refresh();
         }
     }
 }
