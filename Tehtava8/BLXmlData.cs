11bac827ad30a732b9465c8d2b37aaa6a5815dec
mikkoleppanen <mikko.t.leppanen@hotmail.com>
2015-10-08 15:12:39 +0300

    Tehtävä 8 aloitettu
    Ei käänny koska en ehtinyt tehdä valmiiksi

---
+++ Tehtava8/BLXmlData.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Tehtava8
+{
+    class BLXmlData
+    {
+        private List<XmlDataModel> list = new List<XmlDataModel>();
+        public List<XmlDataModel> getDataFromXml()
+        {
+            XElement xmlDoc = new XElement(XDocument.Load("Palautteet2.xml").Root);
+
+            list = (from el in xmlDoc.Descendants("palaute")
+                     select new XmlDataModel
+                     {
+                         date = el.Element("pvm").Value,
+                         name = el.Element("tekija").Value,
+                         learned = el.Element("opittu").Value,
+                         wantToLearn = el.Element("haluanoppia").Value,
+                         good = el.Element("hyvaa").Value,
+                         bad = el.Element("parannettavaa").Value,
+                         other = el.Element("muuta").Value
+                     }).ToList();
+
+            return list;
+        }
+
+        public void writeDataToXml(String name, String learned, String wantToLearn, String good, String bad, String other)
+        {
+            XDocument doc = XDocument.Load("test.xml");
+
+            XmlDataModel tempData = new XmlDataModel
+            {
+                Name = "Coffee restraurant",
+                PhoneNumber = "0xFF",
+                Type = "Coffee shop"
+            };
+            doc.Root.Add(tempData);
+
+            doc.Save("test.xml");
+        }
+    }
+}
---
+++ UnitTestForWinLottoBL/UnitTest1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestForWinLottoBL
+{
+    [TestClass]
+    public class UnitTestLotto
+    {
+        [TestMethod]
+        public void TestGetWeek()
+        {
+            //viittaus oikeaan luokkaan ja sen metodiin
+            JAMK.IT.WinLotto.Lotto lotto = new JAMK.IT.WinLotto.Lotto();
+            //Kutsutaan testattavaa metodia
+            String vk = lotto.GetWeekUnfinished();
+            Assert.AreEqual("41/2015", vk);
+        }
+
+        [TestMethod]
+        public void TestGetWeek2()
+        {
+            //viittaus oikeaan luokkaan ja sen metodiin
+            JAMK.IT.WinLotto.Lotto lotto = new JAMK.IT.WinLotto.Lotto();
+            //Kutsutaan testattavaa metodia
+            String vk = lotto.GetWeekFixed();
+            Assert.AreEqual("41/2015", vk);
+        }
+    }
+}
