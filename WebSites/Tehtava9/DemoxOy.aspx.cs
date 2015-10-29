ff548926ddc80e7bf60912780140a6bb9c4ca871
mikkoleppanen <mikko.t.leppanen@hotmail.com>
2015-10-29 17:04:18 +0200

    Tehtävä 9 toimii nyt oikeasti

--- WebSites/Tehtava9/DemoxOy.aspx.cs
+++ WebSites/Tehtava9/DemoxOy.aspx.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -9,6 +10,27 @@ public partial class DemoxOy : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        GetRealData();
+    }
+    protected void GetDemoData()
+    {
+        DataTable dt = JAMK.IT.DBDemoxOy.GetDataSimple();
+        gvCustomers.DataSource = dt;
+        gvCustomers.DataBind();
+    }
 
+    protected void GetRealData()
+    {
+        try
+        {
+            DataTable dt = JAMK.IT.DBDemoxOy.GetDataReal();
+            gvCustomers.DataSource = dt;
+            gvCustomers.DataBind();
+        }
+        catch (Exception ex)
+        {
+
+            lblNotes.Text = ex.Message;
+        }
     }
 }
